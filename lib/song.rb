9098dfbd784450d6d566b916f7fbb11e6ec4db37
melia miller <meliajm@gmail.com>
2019-12-14 21:49:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
   
   def self.create_from_filename(filename)
     song = self.create 
-    song.name = filename.split("-")[1].split(".")[0].strip
+    song.artist_namename = filename.split("-")[1].split(".")[0].strip
     song
     # binding.pry
     
